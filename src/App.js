2820ced842f04aa7ac660ba6fbf409cebe830ad8
Cesare10-dev <cesare1087@gmail.com>
2022-08-23 09:01:14 +0200

    installed reacticons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.scss";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="box">
         <h2>GitHub clone</h2>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import img1 from "../img/img1.png";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      {/* Left */}
+      <div>
+        <img src={img1} alt="" />
+        <div>
+          <input type="text" placeholder="Search or jump to..." />
+        </div>
+      </div>
+
+      {/* Center */}
+      <div>
+        <h2>Pulls</h2>
+        <h2>Issues</h2>
+        <h2>Marketplace</h2>
+        <h2>Explore</h2>
+      </div>
+      {/* Right */}
+    </div>
+  );
 }
 
 export default Navbar;
